d1f4d76697db9d7ae29b97f132a0ac93da16a18e
Xavoune <lespade.xavier@gmail.com>
2023-08-30 17:47:37 +0200

    intendation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -10,11 +10,11 @@ function getOrderId() {
     // Récupérer le numéro de commande depuis l'URL
     const urlParams = new URLSearchParams(window.location.search);
     const orderId = urlParams.get('id');
-  
+
     // Mettre à jour le contenu de la balise <span> avec le numéro de commande
     const orderIdSpan = document.getElementById('orderId');
     orderIdSpan.textContent = orderId;
-  }
-  
-  // Appeler la fonction d'initialisation au chargement de la page
-  window.onload = getOrderId;
+}
+
+// Appeler la fonction d'initialisation au chargement de la page
+window.onload = getOrderId;
