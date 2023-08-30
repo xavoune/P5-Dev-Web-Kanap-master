dd19d11f49310aaa8ead5333d19b2cfb76ebd4cb
Xavoune <lespade.xavier@gmail.com>
2023-08-30 17:43:51 +0200

    créa page confirmation.js + fonction getOrderId

---
+++ front/js/confirmation.js
@@ -0,0 +1,13 @@
+// Fonction d'initialisation
+function getOrderId() {
+    // Récupérer le numéro de commande depuis l'URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const orderId = urlParams.get('id');
+  
+    // Mettre à jour le contenu de la balise <span> avec le numéro de commande
+    const orderIdSpan = document.getElementById('orderId');
+    orderIdSpan.textContent = orderId;
+  }
+  
+  // Appeler la fonction d'initialisation au chargement de la page
+  window.onload = getOrderId;
